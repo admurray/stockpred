ee726cc20d54e5fd24e5c2bf8b2c48ffba47f943
admurray <ad17yam@gmail.com>
2016-07-10 03:01:03 -0400

    Correct the actual insert statments to insert the actual data.

--- stock_data/stocks.py
+++ stock_data/stocks.py
@@ -5,10 +5,25 @@ Date            : 9th July 2016
 Description     : Manage stocks, and collect information regarding each.
 '''
 
+import json
 from stock_utils import *
+
+
+db_info = client['Amex']
+db = client['Symbols']
+
+collections = ['AMEXSymbols', 'NYSESymbols', 'NASDAQSymbols', 'TSXSymbols']
 
 class Stock:
         '''
         This module works on independent stocks and stores information about
         them
         '''
+        def __init__(self):
+            collection = db['AMEXSymbols']
+            cursor = collection.find()
+            for document in cursor:
+                print document
+
+if __name__ == '__main__':
+    Stock()
